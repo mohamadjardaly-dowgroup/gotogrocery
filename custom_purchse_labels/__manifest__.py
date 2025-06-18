55ed5f8aadd6795321b237bddc8d72a5f31b7a7a
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2025-06-18 17:16:45 +0300

    Add files via upload

---
+++ custom_purchse_labels/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Purchase Quantity Labels',
+    'version': '1.0',
+    'category': 'Purchases',
+    'summary': 'Rename quantity fields in Purchase Orders',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
